66a39919e16770a124401f7be1ad9ceb0a0c5d23
‘Doug <doug.wilhelm@cerner.com>
2018-01-30 13:29:26 -0600

    Reach 'Hello World' Status
    Add project structure to reach proper webpack and React integration, so as to reach 'hello world' status with displaying text from app.jsx through index.html
    
    - app.jsx: Add React render expression for 'hello world' test
    - index.html: Add basic HTML format structure as template
    - package.json: Add: babel presets, react packages, and webpack plugin for compiling HTML
    - webpack.config.js: Correct entry declaration, add resolve settings for modules, compile home page, and add babel React preset

---
+++ src/entry/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+    <p>Hello, world!</p>
+  </div>, document.getElementById('app'),
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,47 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/entry/app.jsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  entry: {
+    app: './src/entry/app.jsx',
   },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[name].js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: ['node_modules'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Eberlywoods',
+      inject: true,
+      template: './src/entry/index.html',
+      filename: 'index.html',
+      chunks: ['app'],
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        test: /\.js$|.jsx$/,
+        exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
     ]
-  }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    stats: 'errors-only',
+    open: true,
+  },
+  devtool: 'inline-source-map',
 };
