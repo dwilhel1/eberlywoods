2248dca6acfe4d6aa05fc5097e2308fb8b3b7fb7
‘Doug <doug.wilhelm@cerner.com>
2018-02-02 15:26:58 -0600

    Add Index Formatting
    Add formatting to index page, add organization for JSX files.
    
    - app.jsx: Rename a few CSS classes
    - index.html: Re-add main div to append files to
    - app.scss: Add index page formatting

--- src/entry/app.jsx
+++ src/entry/app.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Header from '../components/header';
-import Footer from '../components/footer';
+import Header from '../components/layout/Header';
+import Footer from '../components/layout/Footer';
 
 import '../styles/app.scss';
 
@@ -11,40 +11,44 @@ ReactDOM.render(
     <Header />
     <div className={'sustainable-forestry canvas'}>
       <div className={'canvas-text'}>
-        <p>Sustainable Forestry</p>
+        <span>Sustainable Forestry</span>
       </div>
     </div>
-    <div className={'organization description'}>
-      <h2>Who are we?</h2>
-      <p>
-        EberlyWoods is a 4th generation, truly sustainable forest located in Buchanan, Michigan. We also manage or
-        access forests and forestry products in Dowagiac, MI, Elkhart, IN, Bloomington, IN and Bemidji, MN.
-      </p>
-      <p>
-        We are Green Tag Certified and proudly recognized by the National Woodland Owners Association. Timber stand
-        improvement harvests are conducted annually to remove sick, dead, crowded or invasive species, subsequently
-        providing a variety of timber and other ag-forestry grown products.
-      </p>
-      <p>
-        Our products are only from locally grown, truly sustainable forests or urban timber harvests.
-      </p>
+    <div className={'description'}>
+      <div className={'organization text'}>
+        <h2>Who are we?</h2>
+        <p>
+          EberlyWoods is a 4th generation, truly sustainable forest located in Buchanan, Michigan. We also manage or
+          access forests and forestry products in Dowagiac, MI, Elkhart, IN, Bloomington, IN and Bemidji, MN.
+        </p>
+        <p>
+          We are Green Tag Certified and proudly recognized by the National Woodland Owners Association. Timber stand
+          improvement harvests are conducted annually to remove sick, dead, crowded or invasive species, subsequently
+          providing a variety of timber and other ag-forestry grown products.
+        </p>
+        <p>
+          Our products are only from locally grown, truly sustainable forests or urban timber harvests.
+        </p>
+      </div>
     </div>
     <div className={'business-services canvas'}>
       <div className={'canvas-text'}>
-        <p>Lumber Supply</p>
+        <span>Lumber Supply</span>
       </div>
     </div>
-    <div className={'services description'}>
-      <h2>What do we offer?</h2>
-      <p>
-        Services available include quartersawing, slabbing, and flitch cutting up to 56" wide. We also have access to
-        jointing, planing, cross cutting, precision ripping, milling, flooring, kiln drying, and wax coating.
-      </p>
-      <p>
-        All of the lumber services are log-run in order to provide the finest boards from the tree with consistent
-        color-matching and book-matching. Boards are numbered as they finish the milling
-      </p>
+    <div className={'description'}>
+      <div className={'services text'}>
+        <h2>What do we offer?</h2>
+        <p>
+          Services available include quartersawing, slabbing, and flitch cutting up to 56" wide. We also have access to
+          jointing, planing, cross cutting, precision ripping, milling, flooring, kiln drying, and wax coating.
+        </p>
+        <p>
+          All of the lumber services are log-run in order to provide the finest boards from the tree with consistent
+          color-matching and book-matching. Boards are numbered as they finish the milling
+        </p>
+      </div>
     </div>
     <Footer />
-  </div>, document.body
+  </div>, document.getElementById('app')
 );
